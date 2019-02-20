b3db1960f7433b56323a1d83c2c79a640dfd8a48
Aluno <cavanhaman@hotmail.com>
2019-02-19 22:36:25 -0300

    Salvando o relatorio de conexao em arquivo

---
+++ src/pratica3/Servidor.java
@@ -0,0 +1,56 @@
+package pratica3;
+/*
++--------------------+
+| Rodrigo CavanhaMan |
+|        IFTM        |
+|SistemasDistribuídos|
++--------------------+
+*/
+import java.io.*;
+import java.net.*;
+
+public class Servidor {
+    public static void main(String[] args)  {
+        
+        try {
+            
+            InetAddress endereco_remoto;
+            int porta_remota;
+            
+            ServerSocket s = new ServerSocket(2000);//o server socket tem um serviço que espera conexao (implementado abaixo)
+            System.out.println("Esperando conexao..........");
+            
+            Socket conexao = s.accept(); //esta opção faz o servidor ficar rodando esperando conexao
+            System.out.println("Conexao aceita - esperando dados....");
+            
+            endereco_remoto = conexao.getInetAddress();
+            porta_remota = conexao.getPort();
+            
+            FileWriter arq = new FileWriter("d:\\report.txt");
+            PrintWriter gravarArq = new PrintWriter(arq);
+
+            gravarArq.println("Nome da maqina remota: " + endereco_remoto.getHostName());
+            gravarArq.println("IP da maqina remota: " + endereco_remoto.getHostAddress());
+            gravarArq.println("Porta da maqina remota: " + porta_remota);
+            
+            //agora criamos os 2 canais - in e out para conexao (não sobre o s)
+            DataOutputStream saida = new DataOutputStream(conexao.getOutputStream());
+            DataInputStream entrada = new DataInputStream(conexao.getInputStream());
+
+            System.out.println("Conexão realizada com sucesso!");
+            for (int i=0 ; i<200 ; i++){
+                int entra = entrada.readInt();
+                //System.out.println("Entrei");
+                saida.writeUTF("Seu dado foi recebido: "+entra);
+            }
+            
+            arq.close();
+            
+        } catch (IOException ex) {
+            //Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
+        }
+        
+    }
+    
+}
