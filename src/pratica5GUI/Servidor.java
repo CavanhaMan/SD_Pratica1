67124e9d139a9c8278885e1063c6e3b816ec7bb4
Aluno <cavanhaman@hotmail.com>
2019-04-01 20:23:06 -0300

    Interface aviso servidor

--- src/pratica5GUI/Servidor.java
+++ src/pratica5GUI/Servidor.java
@@ -8,14 +8,22 @@ package pratica5GUI;
 *************************/
 import java.io.*;
 import java.net.*;
+import java.text.SimpleDateFormat;
 import java.util.*;
+import javax.swing.JOptionPane;
 
 public class Servidor {
     public static void main(String[] args)  {
         try {
+            Date data = new Date();
+            SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+
             DatagramSocket s = new DatagramSocket(4545);
             System.out.println(aColor.BLUE + aColor.CYAN_BACKGROUND + "___Servidor ativo!_______________________");
             System.out.println(aColor.BLUE + aColor.YELLOW_BACKGROUND + "Servidor aguardando mensagem");
+            System.out.println(aColor.BLUE + aColor.YELLOW_BACKGROUND + "Serviço iniciado em " + formatador.format( data ));
+
+            JOptionPane.showMessageDialog(null,"SERVIDOR ATIVO! \nServiço iniciado em " + formatador.format( data ));
             
             DatagramPacket recebe = new DatagramPacket(new byte[512],512);
             while(true){
